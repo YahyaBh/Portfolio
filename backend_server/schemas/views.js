a7ca1c60fc94501284203eb6c514dcc3dd7dcee8
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2022-11-30 21:34:12 +0100

    m

---
+++ backend_server/schemas/views.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'views',
+    title: 'Views',
+    type: 'document',
+    fields: [
+        {
+            name: 'ip',
+            title: 'Ip',
+            type: 'string',
+        },
+        {
+            name : "country_name",
+            title :"Country_name",
+            type : 'string'
+        },
+        {
+            name : "city",
+            title :"City",
+            type : 'string'
+        }
+    ]
+}
